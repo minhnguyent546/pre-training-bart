fix: wrong condition when printing info about training with mixed precision

bart/run_nmt.py
@@ -106,7 +106,7 @@ def run_nmt(args: argparse.Namespace):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     device = torch.device(device)
     use_fp16 = args.fp16 and device.type == 'cuda'
-    if args.is_master:
+    if use_fp16 and args.is_master:
         print('Training with mixed precision fp16')
 
     checkpoint_states = None